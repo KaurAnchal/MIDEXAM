4707477e41f824729088ed411335d37d64b2da6e
Anchalpreet Kaur <kau15686@sheridancollege.ca>
2024-06-18 17:25:53 -0400

    final

---
+++ src/days/DaysEnum.java
@@ -0,0 +1,15 @@
+package days;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public enum DaysEnum {
+    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
+    
+}
